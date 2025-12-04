163c26a32c9cc3fcd071c6f6f98d98661e1d03f0
Rody <131989043+Justrody@users.noreply.github.com>
2025-12-04 20:47:21 -0300

    Add files via upload

---
+++ src/scripts/utils/profileModal.js
@@ -0,0 +1,13 @@
+const modal = document.getElementById("modal-nav-2");
+const openModalBtb = document.getElementById("open-modal");
+
+openModalBtb.addEventListener("click", (e) => {
+    e.stopPropagation();
+    modal.classList.toggle("hidden");
+})
+document.addEventListener("click", (e) => {
+    if (!modal.contains(e.target) && !openModalBtb.contains(e.target)) {
+        modal.classList.add("hidden");
+    }
+});
+
