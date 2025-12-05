7872d58a11ca120087c57f5cac9cba1789a4616d
Rody <131989043+Justrody@users.noreply.github.com>
2025-12-05 17:36:05 -0300

    Add files via upload

---
+++ src/scripts/utils/profileModal.js
@@ -0,0 +1,39 @@
+const modal = document.getElementById("modal-nav-2");
+const open_modal_btn = document.getElementById("open-modal");
+
+open_modal_btn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    modal.classList.toggle("hidden");
+})
+document.addEventListener("click", (e) => {
+    if (!modal.contains(e.target) && !open_modal_btn.contains(e.target)) {
+        modal.classList.add("hidden");
+    }
+});
+
+const account = document.getElementById("account");
+const open_account_btn = document.getElementById("settings-open-btn");
+const close_account_modal_btn = document.getElementById("close-account-btn");
+
+open_account_btn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    account.classList.toggle("hidden");
+})
+ close_account_modal_btn .addEventListener("click", () => {
+    account.classList.add("hidden");
+}
+);
+
+
+const open_post_upload_modal = document.getElementById("post-content");
+const post_modal = document.getElementById("upload-post-modal");
+const close_upload_modal_btn = document.getElementById("close-upload-btn");
+
+open_post_upload_modal.addEventListener("click", (e) => {
+    e.stopPropagation();
+    post_modal.classList.toggle("hidden");
+})
+close_upload_modal_btn.addEventListener("click", () => {
+    post_modal.classList.add("hidden");
+}
+);
